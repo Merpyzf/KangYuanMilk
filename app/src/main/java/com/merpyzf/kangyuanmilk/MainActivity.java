d57c3de264c88ed4dd7d055454e9f22539268308
wangke <wangke@qq.com>
2017-07-17 11:49:38 +0800

    拉取

--- app/src/main/java/com/merpyzf/kangyuanmilk/MainActivity.java
+++ app/src/main/java/com/merpyzf/kangyuanmilk/MainActivity.java
@@ -2,12 +2,15 @@ package com.merpyzf.kangyuanmilk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
